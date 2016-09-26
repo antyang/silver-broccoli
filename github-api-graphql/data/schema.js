604c37012c69a227ba5102daa15164ab8ede1544
antyang <mrant0ny@gmail.com>
2016-09-26 15:12:39 -0700

    Change the following: * Add UserInfoType queries * Simplify methods for queries

--- github-api-graphql/data/schema.js
+++ github-api-graphql/data/schema.js
@@ -46,15 +46,59 @@ const UserInfoType = new GraphQLObjectType({
     "location": {type: GraphQLString},
     "email": {type: GraphQLString},
     "hireable": {type: GraphQLBoolean},
-    "following_url": {
-       type: GraphQLString,
-       resolve: (obj) => {
-         const brackIndex = obj.following_url.indexOf("{");
-         return obj.following_url.slice(0, brackIndex);
-       }
-     },
+    "following_url": followingUrl(),
+    "users_following": usersFollowing(),
    })
 })
+
+function usersFollowing() {
+  return {
+    type: new GraphQLList(UserInfoType),
+    resolve: (obj) => {
+      const brackIndex = obj.following_url.indexOf("{"),
+            url = obj.following_url.slice(0, brackIndex);
+      return axios.get(url)
+        .then( (res) => {
+          return res.data;
+        });
+    }
+  }
+}
+
+function followingUrl() {
+  return {
+     type: GraphQLString,
+     resolve: (obj) => {
+       const brackIndex = obj.following_url.indexOf("{");
+       return obj.following_url.slice(0, brackIndex);
+     }
+   }
+}
+
+/**
+
+axios.post('/graphql', {
+  query: `
+  {
+    gitHubUser(username: "${login}") {
+      login,
+      following_url,
+      location,
+      users_following {
+        login,
+        id
+      }
+    }
+  }
+  `
+}).then( (res) => {
+  // response from axios comes with a data object and so does graphql
+  // that is why there is data.data
+  // you can call another fn here or if this is in a component you can call
+  // setState.
+  return res.data.data;
+})
+*/
 
 const schema = new GraphQLSchema({
   query
