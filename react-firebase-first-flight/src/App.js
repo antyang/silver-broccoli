67b57a2b513f1309247fa10234c9384b4fda8588
antyang <mrant0ny@gmail.com>
2017-05-13 15:16:56 -0700

    Connect firebase with React

--- react-firebase-first-flight/src/App.js
+++ react-firebase-first-flight/src/App.js
@@ -7,19 +7,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: '',
     };
+
+    this.dataRef = null;
+
+    // binding
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    // whenever the value changes, this will be fired
+    this.dataRef = database.ref('/WOW/LOL/OMG')
+    // whenever the value changes
+    // change the state of the component
+    // set data to current snapshot value
+
+    // this.dataRef.on(...)
+
     database.ref().on('value', (snapshot) => {
       this.setState({
         data: snapshot.val()
       });
       console.log('THE DATA CHANGED!', snapshot.val());
     });
+  }
 
+  handleChange(event) {
+    const newData = event.target.value;
+    console.log('HANDLING CHANGE:', newData);
+    this.setState({
+      newData: newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // this.dataRef.push(...)
+
+    const newData = database.ref('/NEWDATA/this/is/amazing')
+                            // .child('NEWDATA')
+                            .push(this.state.newData);
+    // this.state.newData = '';
+    this.setState({
+      newData: ''
+    })
   }
 
   render() {
@@ -31,6 +65,10 @@ class App extends Component {
         <pre className="App--data">
           { JSON.stringify(this.state.data, null, 2)}
         </pre>
+        <form className="App--form" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
